6f42a87a2c44dfa97357d4b13447c47bae8f54ef
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-03-25 15:37:46 -0600

    Create dummy data

--- app/src/main/java/com/example/mywishlistapp/data/Wish.kt
+++ app/src/main/java/com/example/mywishlistapp/data/Wish.kt
@@ -5,3 +5,14 @@ data class Wish(
     val title: String = "",
     val description: String = ""
 )
+
+object DummyWish{
+    val wishList = listOf(
+        Wish(title = "Steam Deck OLED",
+            description = "A video-games console with the Steam Library.") ,
+        Wish(title = "A trip to Italy",
+        description = "A trip to the city with the most expensive cars in the world."),
+        Wish(title = "100k Job",
+            description = "A job that pays me 100k a month.")
+    )
+}
