46746933963a6f02d8640930ce2b1b8dc4f9996e
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-03-25 15:38:42 -0600

    Display dummy data

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -21,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.mywishlistapp.data.DummyWish
 import com.example.mywishlistapp.data.Wish
 
 @Composable
@@ -50,7 +52,11 @@ fun HomeView(){
         LazyColumn(modifier = Modifier
             .fillMaxSize()
             .padding(it)){
+            items(DummyWish.wishList){
+                wish -> WishItem(wish = wish) {
 
+            }
+            }
         }
     }
 
@@ -58,7 +64,8 @@ fun HomeView(){
 
 @Composable
 fun WishItem(wish: Wish, onClick: () -> Unit){
-    Card(modifier = Modifier.fillMaxWidth()
+    Card(modifier = Modifier
+        .fillMaxWidth()
         .padding(top = 8.dp, start = 8.dp, end = 8.dp)
         .clickable {
             onClick()
