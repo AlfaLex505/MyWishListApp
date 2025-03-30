e652e2bdb0f21e5bc487ebb975698f79324a4247
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-03-30 16:11:05 -0600

    Add the second screen composable and passing the correct arguments to the home view function

--- app/src/main/java/com/example/mywishlistapp/Navigation.kt
+++ app/src/main/java/com/example/mywishlistapp/Navigation.kt
@@ -16,8 +16,13 @@ fun Navigation(viewModel: WishViewModel = viewModel(),
         startDestination = Screen.HomeScreen.route
     ){
         composable(Screen.HomeScreen.route){
-            HomeView()
+            HomeView(navController, viewModel)
         }
+
+        composable(Screen.AddScreen.route){
+            AddEditDetailView(id = 0L, viewModel = viewModel, navController = navController)
+        }
+
     }
 }
 
