426e87683fde2533925c469e60bddd82211550cc
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-04-01 22:49:27 -0600

    Create the  wish repository

---
+++ app/src/main/java/com/example/mywishlistapp/data/Wishrepository.kt
@@ -0,0 +1,25 @@
+package com.example.mywishlistapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class Wishrepository(private val wishDao: WishDao) {
+
+    suspend fun  addWish(wish: Wish){
+        wishDao.addAWish(wish)
+    }
+
+    fun getWishes(): Flow<List<Wish>> = wishDao.getAllWishes()
+
+    fun getAWishByID(id:Long): Flow<Wish>{
+        return wishDao.getAWishById(id)
+    }
+
+    suspend fun updateAWish(wish: Wish){
+        wishDao.updateAWish(wish)
+    }
+
+    suspend fun deleteAWish(wish: Wish){
+        wishDao.deleteAWish(wish)
+    }
+
+}
