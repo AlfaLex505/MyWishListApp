368ef261181af4a29f57728a454c94fd31c86e38
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-04-14 17:57:32 -0600

    Add the function for swipe to dismiss

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -2,6 +2,7 @@ package com.example.mywishlistapp
 
 import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,16 +11,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Card
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -29,6 +38,7 @@ import androidx.navigation.NavController
 import com.example.mywishlistapp.data.DummyWish
 import com.example.mywishlistapp.data.Wish
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView(
     navController: NavController,
@@ -64,10 +74,43 @@ fun HomeView(
             .fillMaxSize()
             .padding(it)){
             items(wishlist.value){
-                wish -> WishItem(wish = wish) {
-                    val id = wish.id
-                    navController.navigate(Screen.AddScreen.route = "/$id")
-                }
+                wish ->
+                val dismissState = rememberDismissState(
+                    confirmStateChange = {
+                        if(it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart){
+                            viewModel.deleteWish(wish)
+                        }
+                        true
+                    }
+                )
+                
+                SwipeToDismiss(
+                    state = dismissState,
+                    background = {
+                         val color by animeteColorAsState(
+                             if(dismissState.dismissDirection
+                                 == DismissDirection.EndToStart Color.Red else Color.Transparent,
+                                 label = "")
+                         )
+                        val alignment = Alignment.CenterEnd
+                        Box(
+                            Modifier.fillMaxSize().background(color).padding(horizontal = 20.dp),
+                            contentAlignment = alignment
+                        ){
+                            Icon(Icons.Default.Delete,
+                                contentDescription = "Delete icon",
+                                tint = Color.White)
+                        }
+
+                    },
+                    directions = setOf(DismissDirection.EndToStart),
+                    dismissContent = {
+                        WishItem(wish = wish) {
+                            val id = wish.id
+                            navController.navigate(Screen.AddScreen.route + "/$id")
+                        }
+                    }
+                    )
             }
         }
     }
