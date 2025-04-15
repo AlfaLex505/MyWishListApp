d77ae56e7e2295917e48bebfaf3518ec029dfe8d
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-04-14 21:22:01 -0600

    Fix bugs on the home view page

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,44 +1,48 @@
 package com.example.mywishlistapp
 
-import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.DismissDirection
 import androidx.compose.material.DismissValue
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SwipeToDismiss
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.rememberDismissState
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.material3.Card
-import androidx.compose.material3.DismissDirection
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.SwipeToDismiss
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.*
+import androidx.compose.ui.draw.scale
 import androidx.navigation.NavController
 import com.example.mywishlistapp.data.DummyWish
 import com.example.mywishlistapp.data.Wish
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeView(
     navController: NavController,
@@ -50,19 +54,18 @@ fun HomeView(
 
     Scaffold(
         scaffoldState = scaffoldState,
-        topBar = { AppBarView(title = "WishList", {
-            Toast.makeText(context, "Button Clicked", Toast.LENGTH_SHORT).show()
-        }) },
+        topBar = { AppBarView(title = "WishList")},
 
         floatingActionButton = {
             FloatingActionButton(
                 modifier = Modifier.padding(all = 20.dp),
                 contentColor = Color.White,
-                containerColor = Color.Black,
-                shape = CircleShape,
+                backgroundColor = Color.Black,
+//                containerColor = Color.Black,
+//                shape = CircleShape,
                 onClick = {
-                    navController.navigate(Screen.AddScreen.route + "0L")
                     Toast.makeText(context, "FAButton Clicked", Toast.LENGTH_LONG).show()
+                    navController.navigate(Screen.AddScreen.route + "/0L")
                 }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
@@ -73,7 +76,7 @@ fun HomeView(
         LazyColumn(modifier = Modifier
             .fillMaxSize()
             .padding(it)){
-            items(wishlist.value){
+            items(wishlist.value, key = {wish -> wish.id}){
                 wish ->
                 val dismissState = rememberDismissState(
                     confirmStateChange = {
@@ -87,10 +90,10 @@ fun HomeView(
                 SwipeToDismiss(
                     state = dismissState,
                     background = {
-                         val color by animeteColorAsState(
+                         val color by animateColorAsState(
                              if(dismissState.dismissDirection
-                                 == DismissDirection.EndToStart Color.Red else Color.Transparent,
-                                 label = "")
+                                 == DismissDirection.EndToStart) Color.Red else Color.Transparent,
+                                 label = ""
                          )
                         val alignment = Alignment.CenterEnd
                         Box(
@@ -104,6 +107,7 @@ fun HomeView(
 
                     },
                     directions = setOf(DismissDirection.EndToStart),
+                    dismissThresholds = {FractionalThreshold(1f)},
                     dismissContent = {
                         WishItem(wish = wish) {
                             val id = wish.id
